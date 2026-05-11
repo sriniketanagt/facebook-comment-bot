ad931dd53aea9ee56097f1820bb5f254941b7275
sriniketanagt <sriniketan.agt@gmail.com>
2026-05-11 13:52:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,62 @@
-async function sendCommentReply(commentId) {
+"use strict";
+
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+
+app.use(express.json());
+
+const VERIFY_TOKEN =
+  process.env.VERIFY_TOKEN || "myverifytoken";
+
+const PAGE_ACCESS_TOKEN =
+  process.env.PAGE_ACCESS_TOKEN;
+
+const PORT =
+  process.env.PORT || 10000;
+
+const repliedComments =
+  new Set();
+
+app.get("/", (req, res) => {
+
+  res.send(
+    "Facebook Comment Bot Running"
+  );
+});
+
+app.get("/webhook", (req, res) => {
+
+  const mode =
+    req.query["hub.mode"];
+
+  const token =
+    req.query["hub.verify_token"];
+
+  const challenge =
+    req.query["hub.challenge"];
+
+  if (
+    mode === "subscribe" &&
+    token === VERIFY_TOKEN
+  ) {
+
+    console.log(
+      "WEBHOOK VERIFIED"
+    );
+
+    return res
+      .status(200)
+      .send(challenge);
+  }
+
+  return res.sendStatus(403);
+});
+
+async function sendCommentReply(
+  commentId
+) {
 
   const url =
     `https://graph.facebook.com/v25.0/${commentId}/comments`;
@@ -17,3 +75,131 @@ async function sendCommentReply(commentId) {
     }
   );
 }
+
+app.post(
+  "/webhook",
+  async (req, res) => {
+
+    console.log(
+      "NEW FACEBOOK EVENT:",
+      JSON.stringify(
+        req.body,
+        null,
+        2
+      )
+    );
+
+    res.sendStatus(200);
+
+    try {
+
+      if (
+        req.body.object !== "page"
+      ) {
+        return;
+      }
+
+      for (
+        const entry of
+        req.body.entry || []
+      ) {
+
+        for (
+          const change of
+          entry.changes || []
+        ) {
+
+          if (
+            change.field !== "feed"
+          ) {
+            continue;
+          }
+
+          const value =
+            change.value || {};
+
+          if (
+            value.item !== "comment" ||
+            value.verb !== "add"
+          ) {
+            continue;
+          }
+
+          const commentId =
+            value.comment_id;
+
+          if (!commentId) {
+            continue;
+          }
+
+          console.log(
+            "COMMENT ID:",
+            commentId
+          );
+
+          if (
+            repliedComments.has(
+              commentId
+            )
+          ) {
+
+            console.log(
+              "ALREADY REPLIED"
+            );
+
+            continue;
+          }
+
+          console.log(
+            "SENDING COMMENT REPLY..."
+          );
+
+          const response =
+            await sendCommentReply(
+              commentId
+            );
+
+          repliedComments.add(
+            commentId
+          );
+
+          console.log(
+            "SUCCESS:",
+            response.data
+          );
+        }
+      }
+
+    } catch (err) {
+
+      console.log(
+        "ERROR SENDING REPLY"
+      );
+
+      if (err.response) {
+
+        console.log(
+          "FACEBOOK ERROR:",
+          JSON.stringify(
+            err.response.data,
+            null,
+            2
+          )
+        );
+
+      } else {
+
+        console.log(
+          err.message
+        );
+      }
+    }
+  }
+);
+
+app.listen(PORT, () => {
+
+  console.log(
+    `SERVER RUNNING ON PORT ${PORT}`
+  );
+});
